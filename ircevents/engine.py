91bd7e3209c7eb11879d251e3f8306326942a829
aewens <email@aewens.com>
2020-03-13 16:14:06 -0500

    Process function can determine which callback to run

--- ircevents/engine.py
+++ ircevents/engine.py
@@ -33,13 +33,13 @@ class Engine:
         """
         """
         self._source = source
-        self._using = list()
-        self._mutations = infinitedict()
+        self._using = set()
 
         self._recv_callback = noop
 
         self._namespaces = set()
         self._whens = set()
+        self._whens_funcs = dict()
         self._whens_namespaces = dict()
         self._whens_map = defaultdict(set)
 
@@ -50,6 +50,7 @@ class Engine:
         """
         Filters out variables from objects into a generator
         """
+
         for attr_name in dir(obj):
             # Ignores all dunder / private attributes
             if attr_name.startswith("_"):
@@ -67,8 +68,9 @@ class Engine:
         """
         Defines the mutations that will be applied to the raw text in `process`
         """
+
         Mutation = namedtuple("Mutation", ["name", "callback"])
-        self._using.append(Mutation(namespace, callback))
+        self._using.add(Mutation(namespace, callback))
 
     def when(self, namespace, **when_kwargs): 
         """
@@ -76,20 +78,61 @@ class Engine:
         The namespace decides what scope of object to pass to callback
         The when keyword arguments determine what will trigger the callback
         """
+
         assert namespace in self._namespaces, f"Invalid namespace: {namespace}"
+
+        # Make hashable for set
         whens = dict2tuple(when_kwargs)
+
+        # Extract unique name
         whens_name = get_class_name(whens)
+
         self._whens.add(whens)
+
+        # Map name to namespace
         self._whens_namespaces[whens_name] = namespace
+
+        # Map keys to name to optimize processing time
         for when_key in whens.keys():
             self._whens_map[when_key].add(whens_name)
 
-        # Pass along function without calling it
-        return lambda func: func
+        def decorator_when(func):
+            # Map name to callback function to run when triggered
+            self._whens_funcs[whens_name] = func
+
+            # Pass along function without calling it
+            return func
+
+        return decorator_when
 
     def process(self, raw_line):
         """
         Applies mutations to the raw IRC text and checks it against callbacks
         """
-        for (key, value} in self._get(variables):
-            NotImplemented
+
+        mutations = dict()
+        requires = dict()
+        triggered_whens = set()
+
+        # Apply mutations
+        for using in self._using:
+            mutation = using.callback(raw_line)
+            mutations[using.namespace] = mutation 
+            for (key, value} in self._get_variables(mutation):
+                for using_whens in self._whens_map[key]:
+                    for using_when in using_whens:
+                        # Already been triggers, skip
+                        if using_when in triggered_whens:
+                            continue
+
+                        # Check if all required fields are found
+                        whens_requires = requires.get(using_when)
+                        if whens_requires is None:
+                            requires[using_whens] = set(self._whens._fields)
+                            whens_requires = requires.get(using_whens)
+
+                        whens_requires.remove(key)
+
+                        # If all requirements are found, trigger callback
+                        if len(whens_requires) == 0:
+                            triggered_whens.add(using_whens)
